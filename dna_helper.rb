eafac8bbd7ae37a4183bd4a22e214f4a2df62a70
James Knight <james.p.knight2@gmail.com>
2019-05-02 14:35:35 -0600

    Update dna_helper.rb
    Removed methods that pertained to mRNA
    Base Pairs added to DNA_helper

--- dna_helper.rb
+++ dna_helper.rb
@@ -1,4 +1,4 @@
-module Dna_helper
+module DNA_helper
   def three_prime
     "3`"
   end
@@ -11,98 +11,19 @@ module Dna_helper
     ' - '
   end
 
-  def mRna_to_amino_acid
+  #Used to complement strands of DNA
+  def dna_base_pair
     {
-        'UUU' => 'Phe',
-        'UUC' => 'Phe',
-        'UUA' => 'Leu',
-        'UUG' => 'Leu',
-
-        'UCU' => 'Ser',
-        'UCC' => 'Ser',
-        'UCA' => 'Ser',
-        'UCG' => 'Ser',
-
-        'UAU' => 'Tyr',
-        'UAC' => 'Tyr',
-        'UAA' =>  nil ,
-        'UAG' =>  nil ,
-
-        'UGU' => 'Cys',
-        'UGC' => 'Cys',
-        'UGA' =>  nil ,
-        'UGG' => 'Trp',
-        #END U
-
-        #BEGIN C
-        'CUU' => 'Leu',
-        'CUC' => 'Leu',
-        'CUA' => 'Leu',
-        'CUG' => 'Leu',
-
-        'CCU' => 'Pro',
-        'CCC' => 'Pro',
-        'CCA' => 'Pro',
-        'CCG' => 'Pro',
-
-        'CAU' => 'His',
-        'CAC' => 'His',
-        'CAA' => 'Gin',
-        'CAG' => 'Gin',
-
-        'CGU' => 'Arg',
-        'CGC' => 'Arg',
-        'CGA' => 'Arg',
-        'CGG' => 'Arg',
-        #END C
-
-        #BEGIN A
-        'AUU' => 'Ile',
-        'AUC' => 'Ile',
-        'AUA' => 'Ile',
-        'AUG' => 'Met',
-
-        'ACU' => 'Thr',
-        'ACC' => 'Thr',
-        'ACA' => 'Thr',
-        'ACG' => 'Thr',
-
-        'AAU' => 'Asn',
-        'AAC' => 'Asn',
-        'AAA' => 'Lys',
-        'AAG' => 'Lys',
-
-        'AGU' => 'Ser',
-        'AGC' => 'Ser',
-        'AGA' => 'Arg',
-        'AGG' => 'Arg',
-        #END A
-
-        #Begin G
-        'GUU' => 'Val',
-        'GUC' => 'Val',
-        'GUA' => 'Val',
-        'GUG' => 'Val',
-
-        'GCU' => 'Ala',
-        'GCC' => 'Ala',
-        'GCA' => 'Ala',
-        'GCG' => 'Ala',
-
-        'GAU' => 'Asp',
-        'GAC' => 'Asp',
-        'GAA' => 'Glu',
-        'GAG' => 'Glu',
-
-        'GGU' => 'Gly',
-        'GGC' => 'Gly',
-        'GGA' => 'Gly',
-        'GGG' => 'Gly'
+        'A' => 'T',
+        'T' => 'A',
+        'G' => 'C',
+        'C' => 'G',
+        '3' => '5',
+        '5' => '3',
+        "`" => "`",
+        "-" => "-",
+        " " => " "
     }
-  end
-
-  def amino_acid_to_mRna
-    mRna_to_amino_acid.invert
   end
 
 end
